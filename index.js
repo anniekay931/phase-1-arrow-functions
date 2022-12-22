cad9458b1a6715763ec141b62cfe56eba4b36774
Annika Kriskovich <annika.krisko2013@gmail.com>
2022-12-22 13:40:34 -0600

    commit tree 7eaf9358c867d7227c3998c5a21438714eb5ff2a

---
+++ index.js
@@ -0,0 +1,14 @@
+// function expression for divide
+const divide = function(a,b) {
+    return a / b;
+}
+
+// arrow function for square
+const square = (x) => {
+    return x * x;
+}
+
+// arrow function for add
+const add = (x, y) => {
+    return x + y;
+}
